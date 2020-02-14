95edb109aa9bb1a91cc760ff09f12fad0dbcbd04
Christoffer Moesgaard <christoffer@magenta.dk>
2020-02-14 11:37:48 +0100

    30861: Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='os2rollekatalog-integration',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Magenta ApS',
+    author_email='',
+    description='',
+    install_requires=[
+        'requests',
+        'os2mo-tools @ git+https://github.com/OS2mo/os2mo-tools.git@0.2.0'
+    ]
+)
